c0fd8c307552045871963ec09bc12580db59a7ff
johntheman54321 <john.wahba09@gmail.com>
2024-05-13 23:19:50 -0400

    Added submarine and fixed player movement

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,9 +1,19 @@
 extends CharacterBody2D
+class_name Player
 
 @onready var player = $"."
-@onready var look_at_angle = $look_at_angle
+@export var speed = 7
+var dir : Vector2
 
 
 
 func _process(delta):
+	dir = Vector2(get_global_mouse_position() - position).normalized()
+	print(position)
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		position += (dir * speed)
+	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		position += (dir * (speed + 6))
 	player.look_at(get_global_mouse_position())
+	
+	move_and_slide()
