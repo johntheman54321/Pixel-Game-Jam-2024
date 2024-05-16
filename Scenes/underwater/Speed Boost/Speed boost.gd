22e45d48ee19d40986715b485e272ef08c8eea8e
ugihu <abraamzahian11@gmail.com>
2024-05-16 19:50:16 -0400

    couldn't figure out how to do map 2
    and code didn't work eiter idk why

--- Scenes/underwater/Speed Boost/Speed boost.gd
+++ Scenes/underwater/Speed Boost/Speed boost.gd
@@ -1,6 +1,5 @@
 extends Node2D
-
-
+@onready var world = get_parent()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,3 +9,11 @@ func _ready():
 func _process(delta):
 	pass
  
+func _on_area_2d_body_entered(body):
+	if body is Player:
+		body.speed = 700
+		world.timer.start()
+		queue_free()
+func _on_timer_timeout():
+	$CharacterBody2D.speed = 400
+	print("pee")
